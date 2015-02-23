24f6f7ec03089d9d6292c3ac9ee172a59df7c547
Jordan Schreuder <schreuderjo@gmail.com>
2015-02-23 13:47:07 -0600

    add player functionality done

--- js-memory-game.js
+++ js-memory-game.js
@@ -1,17 +1,30 @@
 Deck = new Mongo.Collection("cards");
+Players = new Mongo.Collection("players");
 
 function Game(){
   this.started = false;
+  this.players = 0;
+  this.solved = false;
+  this.winner;
+}
+
+function Player(name){
+  this.name = name;
+  this.score = 0;
 }
 
 var futuramaDeck = ["Bender", "Leela", "Fry", "Zoidberg", "Amy", "Professor", "Zapp Brannigan", "Hermes", "Kiff", "Scruffy", "Nibbler"];
 
 if (Meteor.isClient) {
   Meteor.subscribe("cards");
+  Meteor.subscribe("players");
 
   Template.body.helpers({
     cards: function () {
       return Deck.find({});
+    },
+    players: function() {
+      return Players.find({});
     },
     gameStarted: function(){
       return currentGame.started;
@@ -21,11 +34,17 @@ if (Meteor.isClient) {
   Template.body.events({
     "click .new-game": function (event) {
       var currentGame = new Game();
-      console.log("Let the games begin!!!");
-      console.log(currentGame.started);
       currentGame.started = true;
-      console.log(currentGame.started);
-      // return false;
+      return false;
+    }
+  });
+
+  Template.addPlayerForm.events({
+    "submit form": function(event) {
+      event.preventDefault();
+      var playerName = event.target.playerName.value;
+      Meteor.call("addPlayer", playerName);
+      event.target.playerName.value = "";
     }
   });
 }
@@ -40,9 +59,21 @@ if (Meteor.isServer) {
   Meteor.publish("cards", function () {
     return Deck.find();
   });
+
+  Meteor.publish("players", function () {
+    return Players.find();
+  });
 }
 
 Meteor.methods({
+  addPlayer: function(name) {
+    Players.insert({
+      name: name,
+      score: 0,
+      createdAt: new Date()
+    });
+  },
+
   createDeck: function(cards){
     for (var i=0; i<cards.length; i++) {
       Deck.insert({
